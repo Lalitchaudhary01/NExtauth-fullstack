141bdfd1bc26a309a607d155a7a3ecd201d0b654
Lalitchaudhary01 <lalitkumar@gmail.com>
2025-06-18 17:18:37 +0530

    connect mongoDB

--- models/Video.ts
+++ models/Video.ts
@@ -20,15 +20,22 @@ export interface IVideo {
   createdAt?: Date;
   updatedAt?: Date;
 }
-const videoSchema = new Schema<IVideo>({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  videoUrl: { type: String, required: true },
-  thumbnailUrl: { type: String, required: true },
-  controls: { type: Boolean, default: true },
-  transfomation: {
-    width: { type: Number, default: VIDEO_DIMENSIONS.width },
-    height: { type: Number, default: VIDEO_DIMENSIONS.height },
-    quality: { type: Number, min: 1, max: 100 },
+const videoSchema = new Schema<IVideo>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    controls: { type: Boolean, default: true },
+    transfomation: {
+      width: { type: Number, default: VIDEO_DIMENSIONS.width },
+      height: { type: Number, default: VIDEO_DIMENSIONS.height },
+      quality: { type: Number, min: 1, max: 100 },
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+const Video = models?.Video || model<IVideo>("Video", videoSchema);
+export default Video;
